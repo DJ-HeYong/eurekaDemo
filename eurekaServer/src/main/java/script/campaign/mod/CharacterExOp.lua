6fe0978023b03a36134129db95b5eaa3c1de3e93
DJ-HeYong <3426927098@qq.com>
2022-01-20 15:58:28 +0800

    拆分

--- eurekaServer/src/main/java/script/campaign/mod/CharacterExOp.lua
+++ eurekaServer/src/main/java/script/campaign/mod/CharacterExOp.lua
@@ -91,61 +91,69 @@ local function solidify_loyalty(modify_character, modify_faction)
     ModLog("FactionEffectBundleAwarded--执行, 【巩固忠诚】，国库减少300");
 end
 
+local function character_ex_op_do(context)
+    ModLog("------------分割线------------------- ");
+    ModLog("------------分割线------------------- ");
+    ModLog("------------分割线------------------- ");
+    --1、为什么使用FactionEffectBundleAwarded？？？ 因为没找到其他更好的办法！
+    --2、effect_bundle_key 只是用来给势力，和武将添加buff的，为了配合"FactionEffectBundleAwarded"事件监听，从而知道鼠标点击的按钮对应哪个武将。
+    --3、campaign_payload_effect_bundles_tables中，同一个payload对应的effectBundle必须character排在faction之上（按照顺序依次赋予effectBundle，
+    --因为我们触发"FactionEffectBundleAwarded"时候，必须保证effectBundle已经赋予给了character，否则无法获取点击了哪个武将）
+    --4、为了不影响下一次按钮的点击，我们每次都【必须】清除faction和character的effect_bundle
+
+    local effect_bundle_key_faction = context:effect_bundle_key()
+    local effect_bundle_key_character = get_effect_bundle_key_character(effect_bundle_key_faction)
+    local query_faction = context:faction()
+    local modify_faction = cm:modify_faction(query_faction)
+    --先移除势力的effect_bundle，每次【必须】移除
+    modify_faction:remove_effect_bundle(effect_bundle_key_faction)
+    local character_list = query_faction:character_list()
+    for i = 0, character_list:num_items() - 1 do
+        local query_character = character_list:item_at(i)
+        if ((not query_character:is_dead()) and query_character:has_effect_bundle(effect_bundle_key_character)) then
+            local character_template_key = query_character:generation_template_key();
+            local cqi = query_character:cqi();
+            local modify_character = cm:modify_character(cqi)
+            --先移除武将身上的effect_bundle，每次【必须】移除
+            modify_character:remove_effect_bundle(effect_bundle_key_character);
+            ModLog("FactionEffectBundleAwarded--执行start, 目标人物: " .. character_template_key);
+            --根据类型，调用不同的方法
+            if (effect_bundle_key_character == "effect_bundle_斩首character") then
+                kill(modify_character, modify_faction)
+            elseif (effect_bundle_key_character == "effect_bundle_纳妻character") then
+                CharacterExOp_marry_byHy:marry_wife(query_character, modify_character, query_faction, modify_faction)
+            elseif (effect_bundle_key_character == "effect_bundle_纳妾character") then
+                CharacterExOp_marry_byHy:marry_concubine(query_character, modify_character, query_faction, modify_faction)
+            elseif (effect_bundle_key_character == "effect_bundle_结拜character") then
+                brother(query_character, modify_character, query_faction, modify_faction)
+            elseif (effect_bundle_key_character == "effect_bundle_巩固忠诚character") then
+                solidify_loyalty(modify_character, modify_faction)
+            end
+            --由于上面character每次都会移除effect_bundle，所以此处只需要检测到一个符合条件的character即可，然后break跳出循环
+            break ;
+        end
+    end
+    ModLog("FactionEffectBundleAwarded--执行over, 目标人物: " .. character_template_key);
+end
+
+
 --==============================================================================--
 -- 监听函数 --
 --==============================================================================--
-local function characterExOp_byHy()
+local function character_ex_op_byHy()
     core:add_listener(
             listen_name,
-            "FactionEffectBundleAwarded",
+            "FactionEffectBundleAwarded", --势力获得EffectBundle时候触发监听
             function(context)
                 return is_contain_effect_bundle_key_faction(context:effect_bundle_key())
             end,
             function(context)
-                ModLog("------------分割线------------------- ");
-                ModLog("------------分割线------------------- ");
-                ModLog("------------分割线------------------- ");
-                --为什么使用FactionEffectBundleAwarded？？？ 因为没找到其他更好的办法！
-                --effect_bundle_key 只是用来给势力，和武将添加buff的，为了配合"FactionEffectBundleAwarded"事件监听，从而知道鼠标点击的按钮对应哪个武将。
-                --为了不影响下一次按钮的点击，所以我们每次都要清除faction和character的effect_bundle_key
-
-                local effect_bundle_key_faction = context:effect_bundle_key()
-                local effect_bundle_key_character = get_effect_bundle_key_character(effect_bundle_key_faction)
-                local query_faction = context:faction()
-                local modify_faction = cm:modify_faction(query_faction)
-                --先移除势力的effect_bundle，每次必须移除
-                modify_faction:remove_effect_bundle(effect_bundle_key_faction)
-                local character_list = query_faction:character_list()
-                for i = 0, character_list:num_items() - 1 do
-                    local query_character = character_list:item_at(i)
-                    if ((not query_character:is_dead()) and query_character:has_effect_bundle(effect_bundle_key_character)) then
-                        local character_template_key = query_character:generation_template_key();
-                        local cqi = query_character:cqi();
-                        local modify_character = cm:modify_character(cqi)
-                        --先移除武将身上的effect_bundle，每次必须移除
-                        modify_character:remove_effect_bundle(effect_bundle_key_character);
-                        ModLog("FactionEffectBundleAwarded--执行, 目标人物: " .. character_template_key);
-                        --根据类型，调用不同的方法
-                        if (effect_bundle_key_character == "effect_bundle_斩首character") then
-                            kill(modify_character, modify_faction)
-                        elseif (effect_bundle_key_character == "effect_bundle_纳妻character") then
-                            CharacterExOp_marry_byHy:marry_wife(query_character, modify_character, query_faction, modify_faction)
-                        elseif (effect_bundle_key_character == "effect_bundle_纳妾character") then
-                            CharacterExOp_marry_byHy:marry_concubine(query_character, modify_character, query_faction, modify_faction)
-                        elseif (effect_bundle_key_character == "effect_bundle_结拜character") then
-                            brother(query_character, modify_character, query_faction, modify_faction)
-                        elseif (effect_bundle_key_character == "effect_bundle_巩固忠诚character") then
-                            solidify_loyalty(modify_character, modify_faction)
-                        end
-                        --由于上面character每次都会移除effect_bundle，所以此处只需要检测到一个符合条件的character即可，然后break跳出循环
-                        break ;
-                    end
-                end
+                character_ex_op_do(context)
             end,
             true
     )
 end
 
 cm:add_first_tick_callback(function()
-    characterExOp_byHy()
+    character_ex_op_byHy()
 end)
